8640d9766745a5df4d5f8144f0f3aae49525141b
Prince Codes <princecodes247@gmail.com>
2022-06-15 14:26:20 -0700

    fix

--- src/config/index.js
+++ src/config/index.js
@@ -7,6 +7,17 @@ const environments = {
     API_URL: "http://localhost:3000",
     API_VERSION: "v1",
     MONGODB_URI: process.env.MONGODB_URI || "mongodb://localhost:27017/",
+    JWT_SECRET: "XXXX-XXXX-XXXX",
+    BCRYPT_SALT: 10,
+    role: {
+      ADMIN: ["admin"],
+      USER: ["user", "admin"],
+    },
+
+    url: {
+      CLIENT_URL: "http://localhost:8080",
+      BASE_URL: "http://localhost:8080",
+    },
   },
 };
 
