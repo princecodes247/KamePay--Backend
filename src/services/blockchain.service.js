0312cde94b4fadd039ba66a9f5c26cd76faa2772
Prince Codes <princecodes247@gmail.com>
2022-06-15 15:25:25 -0700

    update blockchain service

--- src/services/blockchain.service.js
+++ src/services/blockchain.service.js
@@ -2,7 +2,11 @@ const User = require("./../models/user.model");
 const CustomError = require("./../utils/custom-error");
 const Web3 = require("web3");
 
-const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+const web3 = new Web3(
+  new Web3.providers.HttpProvider(
+    "https://eth-rinkeby.alchemyapi.io/v2/MCv-KDVuqfpEhd6IcfzssNUM-uQFYpJt"
+  )
+);
 class BlockchainService {
   async createAddress() {
     const account = web3.eth.accounts.create();
@@ -14,6 +18,7 @@ class BlockchainService {
 
   async getBalance(address) {
     const balance = web3.eth.getBalance(address);
+    console.log(balance, "BlockchainService.getBalance");
     return balance;
   }
 }
