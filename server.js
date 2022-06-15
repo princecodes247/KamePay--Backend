cd6b0e2b459bdaf5f80af1a0ce7521dc178c25a3
Prince Codes <princecodes247@gmail.com>
2022-06-15 14:35:21 -0700

    test

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require("express-async-errors");
 const app = require("express")();
 const { PORT } = process.env;
 
-
 // Pre-route middlewares
 require("./src/middlewares/pre-route.middleware")(app);
 
@@ -13,19 +12,20 @@ app.use("/api", require("./src/routes"));
 app.get("/ping", (req, res) => res.status(200).send("Hello world!"));
 
 // Error middlewares
-require("./src/middlewares/error.middleware")(app);
+// require("./src/middlewares/error.middleware")(app);
 
 // Listen to server port
 app.listen(PORT, async () => {
-     //Initialize MongoDB
-     await require("./src/config/mongo-db.config")()
-     console.log(`:::> Server listening on port ${PORT} @ http://localhost:${PORT}`);
+  //Initialize MongoDB
+  await require("./src/config/mongo-db.config")();
+  console.log(
+    `:::> Server listening on port ${PORT} @ http://localhost:${PORT}`
+  );
 });
 
 // On  server error
 app.on("error", (error) => {
-     console.error(`<::: An error occurred on the server: \n ${error}`);
+  console.error(`<::: An error occurred on the server: \n ${error}`);
 });
 
-
-module.exports = app
+module.exports = app;
