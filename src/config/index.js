2ca068bc7a7173746e1109402c723ceb34685758
Prince Codes <princecodes247@gmail.com>
2022-06-15 14:22:37 -0700

    fix

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,14 @@
-const env = process.env.NODE_ENV
+const env = process.env.NODE_ENV;
 
 const environments = {
-     development: require("./env/dev.env.json"),
-     production: require("./env/prod.env.json")
-}
-
+  development: require("./env/dev.env.json"),
+  // production: require("./env/prod.env.json")
+  production: {
+    API_URL: "http://localhost:3000",
+    API_VERSION: "v1",
+    MONGODB_URI: process.env.MONGODB_URI || "mongodb://localhost:27017/",
+  },
+};
 
 // export config for the current environment
-module.exports = environments[env] || environments["production"] 
+module.exports = environments[env] || environments["production"];
